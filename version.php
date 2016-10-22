0ccd15e0d032308c7ceb6500c79eb9f8d68558e5
valery <valery.fremaux@gmail.com>
2016-10-22 23:27:51 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016062300;
-$plugin->requires  = 2014110400;
+$plugin->requires  = 2015050500;
 $plugin->component = 'quizaccess_chooseconstraints';
-$plugin->release = '2.8.0 Build(2016062300)';
+$plugin->release = '2.9.0 Build(2016062300)';
 $plugin->maturity = MATURITY_BETA;
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.8.0000';
+$plugin->codeincrement = '2.9.0000';
